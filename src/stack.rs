451650186ca5766d8d2824674247cc2b728dcf2d
cod10129 <110200933+cod10129@users.noreply.github.com>
2023-06-13 13:26:24 -0500

    stack::IntoIter tests

--- src/stack.rs
+++ src/stack.rs
@@ -311,4 +311,34 @@ mod tests {
         assert_eq!(iter.next(), Some(&mut 11));
         assert_eq!(iter.next(), None);
     }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn for_loop() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut i = 1;
+
+        for elem in list {
+            assert_eq!(elem, i);
+            i += 1;
+        }
+    }
 }
