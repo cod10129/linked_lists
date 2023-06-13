4ec40f3c31eb49348004575825e8bf32d0375e42
cod10129 <110200933+cod10129@users.noreply.github.com>
2023-06-13 11:47:30 -0500

    changed grammar in stack docs

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,4 @@
-//! A singly linked list, with stack operations.
+//! A singly linked list with stack operations.
 use alloc::boxed::Box;
 
 /// A singly linked list that can do (most of) what a stack can.
